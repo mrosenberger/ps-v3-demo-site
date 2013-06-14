301dce5b4a57f528b06c852c7a4d70ae02e84a2e
Michael Rosenberger <leahcim165@gmail.com>
2013-06-14 10:08:09 -0700

    Created a nice bootstrap test page

---
+++ bootstrap-test.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>ShopFoo</title>
+    <style>
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+	<div class="container">
+	  <a class="brand">ShopFoo</a>
+	  <div class="nav-collapse collapse">
+	    <ul class="nav">
+	      <li><a>Departments</a></li>
+	      <li><a>Stores</a></li>
+	      <li><a>Coupons</a></li>
+	      <li><a>About</a></li>
+	    </ul>
+	    <form class="navbar-form pull-right">
+	      <input class="span2" type="text" placeholder="Search products...">
+	      <button type="submit" class="btn">Go</button>
+	    </form>
+	    <select class="span2 pull-right">
+	      <option>First</option>
+	      <option>Second</option>
+	    </select>
+	  </div>
+	</div>
+      </div>
+    </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>	     
+</html>
---
+++ views/layouts/navbar.php
@@ -0,0 +1,35 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="#">ShopFoo</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+          <li><a href="views/products/index.php">Products</a></li>
+          <li><a href="views/merchants/index.php">Merchants</a></li>
+          <li><a href="views/categories/index.php">Categories</a></li>
+          <!--<li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="nav-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+          </li>-->
+        </ul>
+        <form class="navbar-form pull-right" action="">
+          <input class="span2" type="text" placeholder="Search products...">
+          <button type="submit" class="btn">Go!</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
