571fb0139fa8b472b7ebdcfcd2c94c36278d4765
Michael Rosenberger <leahcim165@gmail.com>
2013-06-20 10:06:18 -0700

    Separated navbar, header, and bottom js includes into seperate files

---
+++ header-titleless.php
@@ -0,0 +1,25 @@
+<style>
+    body {
+	padding-top: 60px;
+        padding-bottom: 40px;
+    }
+    hr {
+	color: #000000;
+        background-color: #000000;
+        height: 2px;
+    }
+    </style>
+    <?php require 'render-product.php' ?>
+    <?php require 'ps-v3-library.php' ?>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <style>
+	.sidebar {
+	    margin-left: 0;
+	    padding-right: 19px;
+	    border-right: 1px groove #000000;
+	    min-height: 768px;
+	}
+    </style>
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
--- navbar.php
+++ navbar.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      hr {
-        color: #000000;
-        background-color: #000000;
-        height: 2px;
-      }
-    </style>
-    <?php require 'render-product.php' ?>
-    <?php require 'ps-v3-library.php' ?>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <style>
-      .sidebar {
-	margin-left: 0;
-	padding-right: 19px;
-	border-right: 1px groove #000000;
-	min-height: 768px;
-      }
-    </style>
-    <title>ShopFoo</title>
-    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-  </head>
-  <body>
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
 	<div class="container">
@@ -79,42 +48,3 @@
 	</div>
       </div>
     </div>
-    <div class="container">
-      <div class="row">
-        <div class="span2 sidebar">
-          Categories:
-          <ul>
-	    <li>Something</li>
-	    <li>Something</li>
-	    <li>Something</li>
-	    <li>Something</li>
-	  </ul>
-        </div>
-        <div class="span10">
-        <?php
-        $api = new PsApiCall(array(
-				   'account' => 'd1lg0my9c6y3j5iv5vkc6ayrd',
-				   'catalog' => 'dp4rtmme6tbhugpv6i59yiqmr',
-				   'logging' => false
-				   ));
-        $valid_options = array('category', 'include_discounts', 'keyword', 'keyword_description', 'keyword_ean', 'keyword_isbn', 'keyword_mpn', 'keyword_name', 'keyword_person', 'keyword_upc', 'keyword_sku', 'merchant', 'merchant_type', 'page', 'percent_off', 'percent_off_max', 'percent_off_min', 'postal_code', 'price', 'price_max', 'product', 'product_spec', 'include_identifiers', 'results_per_page', 'session', 'tracking_id');
-        $parameters = array();
-        foreach ($_GET as $key=>$value) {
-	  if (in_array($key, $valid_options)) {
-	    $parameters[$key] = $value;
-	  }
-	}
-        $api->get('products', $parameters);
-        foreach ($api->resource('products') as $product) {
-	  renderProduct($product);
-	}
-        ?>
-	</div>
-      </div>
-    </div>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-    <script src="js/vendor/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-  </body>
-</html>
