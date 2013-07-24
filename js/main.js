73c8996d377a4a4044ac65f489ba5e95bde642c6
Michael Rosenberger <leahcim165@gmail.com>
2013-07-24 13:36:20 -0700

    Moved javascript to main.js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+(function ( $, window, document, undefined ) {
+    var toggleChildren = function(event, hideSelector, showSelector){
+        var $container = $(event.target).parents(".more-less:first");
+        $container.find(hideSelector).hide();
+        $container.find(showSelector).show();
+    };
+    $(".read-more").click(function(event){
+        event.preventDefault();
+        toggleChildren(event, '.less', '.more');
+    });
+    $(".read-less").click(function(event){
+        event.preventDefault();
+        toggleChildren(event, '.more', '.less');
+    });
+})( jQuery, window, document );
